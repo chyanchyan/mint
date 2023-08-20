3bdcf4782547861e390d6e9ccd355db09b844328
chyanchyan <zhou_qianchi@163.com>
2023-08-21 04:06:20 +0800

    reframe table objs

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, Float, String, BINARY, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base
+from datetime import datetime as dt
+
+
+Base = declarative_base()
+
+# table class start
+
+# table class end
