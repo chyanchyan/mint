c1ad17df18e776d7e5ef4ffb4307cd4cc21a2846
zhou <zhouqianchi@duxiaoman.com>
2024-04-25 15:23:11 +0800

    coding format added

--- api_functions.py
+++ api_functions.py
@@ -441,7 +441,7 @@ def gen_booking_xl_sheet_file(root, row_id=''):
     template_path = os.path.join(PATH_ROOT, 'templates', 'booking_xl_template.xlsm')
     output_folder = os.path.join(PATH_OUTPUT, 'booking_xl_sheet')
     mkdir(output_folder)
-    output_filename = f'booking_excel-{p_name}-{timestamp}.xlsm'
+    output_filename = f'booking_excel-{timestamp}.xlsm'
     output_path = os.path.join(
         output_folder,
         output_filename
