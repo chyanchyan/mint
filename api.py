0b07abc2e5e3a5045d6b33fca85a719a6d20a264
zhou <zhouqianchi@duxiaoman.com>
2024-05-13 07:29:21 +0800

    upload api added

--- api.py
+++ api.py
@@ -3,14 +3,17 @@ import os
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 import urllib.parse
-
+from werkzeug.utils import secure_filename
 
 if 'mint' in __name__.split('.'):
     from .helper_function.hf_string import to_json_str
     from .helper_function.wrappers import api_status_wrapper, sql_retry_wrapper
+    from .sys_init import *
 else:
     from helper_function.hf_string import to_json_str
     from helper_function.wrappers import api_status_wrapper, sql_retry_wrapper
+    from sys_init import *
+
 
 app = Flask(__name__)
 CORS(app=app)
@@ -42,6 +45,26 @@ def api_file_download(file_path):
     return res
 
 
+@app.route('/api/fileUpload', methods=['get', 'post'])
+@api_status_wrapper
+def api_file_upload():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    if file:
+        filename = file.filename
+        # filename = secure_filename(file.filename)
+        filepath = os.path.join(PATH_UPLOAD, filename)
+        file.save(filepath)
+        print(f'file saved: {filepath}')
+        return {'fileName': filename, 'filePath': filepath}
+    else:
+        return {'error': 'File type not allowed'}
 
 
 @app.route('/api/hello', methods=['GET', 'POST'])
